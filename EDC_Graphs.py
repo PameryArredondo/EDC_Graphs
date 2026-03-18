6580479e44e73f09fc5bd3467fd53a1ebb8940c0
PameryArredondo <Pamery@validatedcs.com>
2026-03-18 17:08:07 +0000

    renamed uploaded.name to file_name

--- EDC_Graphs.py
+++ EDC_Graphs.py
@@ -3160,10 +3160,10 @@ def main():
 
     if file_format == "monaderm":
         st.success("📡 Monaderm RAW DATA format detected.")
-        run_monaderm_flow(file_bytes=file_bytes, file_name=uploaded.name)
+        run_monaderm_flow()
     else:
         st.success("📋 eCRF format detected.")
-        run_excel_flow(file_bytes=file_bytes, file_name=uploaded.name)
+        run_excel_flow(file_bytes=file_bytes, file_name=file_name)
 
 
 if __name__ == "__main__":
